88c95f824344d136b1236a3032e30bbca95e8f22
Xiang Wu <xiang.wu@hotmail.de>
2023-07-17 22:37:24 +0200

    grafbase config docu

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,25 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+const User = g.model('User', {
+  name: g.string(),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(()=> Project).list().optional(),
+
+  // Extend models with resolvers
+  // https://grafbase.com/docs/edge-gateway/resolvers
+  // gravatar: g.url().resolver('user/gravatar')
+})
+
+const Project = g.model('Project',{
+  title: g.string(),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createBy: g.relation(()=> User)
+})
